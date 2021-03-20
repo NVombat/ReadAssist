df3da5290c615fc799c5b7ff5dba163904a98368
sanahsidhu <sanaa.02.march@gmail.com>
2021-03-20 12:06:37 +0530

    feat(*): database code added

---
+++ models/summ.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+def summary_tbl():
+    conn = sqlite3.connect("app.db")
+    cur = conn.cursor()
+
+
+    sql = 'CREATE TABLE IF NOT EXISTS summary_tbl(Email TEXT, Summary TEXT)'
+    cur.execute(sql)
+    conn.commit()
+
+def insert(user_email_id : str, summary_text: str):
+    conn = sqlite3.connect("app.db")
+    cur = conn.cursor()
+
+    sql = f"INSERT INTO summary_tbl VALUES('{user_email_id}','{summary_text}')"   
+    cur.execute(sql)
+    conn.commit()
+
+def getq(user_email : str):
+    conn = sqlite3.connect("app.db")
+    cur = conn.cursor()
+
+    sql = f"SELECT Summary FROM summary_tbl WHERE Email='{user_email}' "
+    cur.execute(sql)
+    fsummary = cur.fetchall()
+    
+    return fsummary
