266bfece261b8328848b555d67246550304e0f4a
SanahSidhu <67470527+SanahSidhu@users.noreply.github.com>
2021-03-21 03:08:42 +0530

    Update summ.py

--- models/summ.py
+++ models/summ.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+#function for creating summary_tbl
 def summary_tbl(path : str):
     conn = sqlite3.connect(path)
     cur = conn.cursor()
@@ -8,7 +9,8 @@ def summary_tbl(path : str):
     sql = 'CREATE TABLE IF NOT EXISTS summary_tbl(Email TEXT, Summary TEXT)'
     cur.execute(sql)
     conn.commit()
-
+    
+#function for inserting values into the summary_tbl
 def insert(user_email_id : str, summary_text: str):
     conn = sqlite3.connect("app.db")
     cur = conn.cursor()
@@ -17,6 +19,7 @@ def insert(user_email_id : str, summary_text: str):
     cur.execute(sql)
     conn.commit()
 
+#function that returns the final summary from table where email = currently logged in user
 def getq(user_email : str):
     conn = sqlite3.connect("app.db")
     cur = conn.cursor()
