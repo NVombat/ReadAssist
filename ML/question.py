fe4ffc8d0bacb5f8998b870aa2947ef6d4a66191
SanahSidhu <sanaa.02.march@gmail.com>
2021-03-20 13:44:40 +0530

    feat(*): email and quest lib

--- ML/question.py
+++ ML/question.py
@@ -194,4 +194,165 @@ class MultiTaskQAQGPipeline(QGPipeline):
         return answer
 
 
-        
+class E2EQGPipeline:
+    def __init__(self, model: PreTrainedModel, tokenizer: PreTrainedTokenizer, use_cuda: bool):
+
+        self.model = model
+        self.tokenizer = tokenizer
+
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model.to(self.device)
+
+        assert self.model.__class__.__name__ in ["T5ForConditionalGeneration", "BartForConditionalGeneration"]
+
+        if "T5ForConditionalGeneration" in self.model.__class__.__name__:
+            self.model_type = "t5"
+        else:
+            self.model_type = "bart"
+
+        self.default_generate_kwargs = {
+            "max_length": 256,
+            "num_beams": 4,
+            "length_penalty": 1.5,
+            "no_repeat_ngram_size": 3,
+            "early_stopping": True,
+        }
+
+    def __call__(self, context: str, **generate_kwargs):
+        inputs = self._prepare_inputs_for_e2e_qg(context)
+
+        if not generate_kwargs:
+            generate_kwargs = self.default_generate_kwargs
+
+        input_length = inputs["input_ids"].shape[-1]
+
+        outs = self.model.generate(
+            input_ids=inputs["input_ids"].to(self.device),
+            attention_mask=inputs["attention_mask"].to(self.device),
+            **generate_kwargs,
+        )
+
+        prediction = self.tokenizer.decode(outs[0], skip_special_tokens=True)
+        questions = prediction.split("<sep>")
+        questions = [question.strip() for question in questions[:-1]]
+        return questions
+
+    def _prepare_inputs_for_e2e_qg(self, context):
+        source_text = f"generate questions: {context}"
+        if self.model_type == "t5":
+            source_text = source_text + " </s>"
+
+        inputs = self._tokenize([source_text], padding=False)
+        return inputs
+
+    def _tokenize(self, inputs, padding=True, truncation=True, add_special_tokens=True, max_length=512):
+        inputs = self.tokenizer.batch_encode_plus(
+            inputs,
+            max_length=max_length,
+            add_special_tokens=add_special_tokens,
+            truncation=truncation,
+            padding="max_length" if padding else False,
+            pad_to_max_length=padding,
+            return_tensors="pt",
+        )
+        return inputs
+
+
+SUPPORTED_TASKS = {
+    "question-generation": {
+        "impl": QGPipeline,
+        "default": {"model": "valhalla/t5-small-qg-hl", "ans_model": "valhalla/t5-small-qa-qg-hl",},
+    },
+    "multitask-qa-qg": {"impl": MultiTaskQAQGPipeline, "default": {"model": "valhalla/t5-small-qa-qg-hl",}},
+    "e2e-qg": {"impl": E2EQGPipeline, "default": {"model": "valhalla/t5-small-e2e-qg",}},
+}
+
+
+def secondary_pipeline(
+    task: str,
+    model: Optional = None,
+    tokenizer: Optional[Union[str, PreTrainedTokenizer]] = None,
+    qg_format: Optional[str] = "highlight",
+    ans_model: Optional = None,
+    ans_tokenizer: Optional[Union[str, PreTrainedTokenizer]] = None,
+    use_cuda: Optional[bool] = True,
+    **kwargs,
+):
+    # Retrieve the task
+    if task not in SUPPORTED_TASKS:
+        raise KeyError("Unknown task {}, available tasks are {}".format(task, list(SUPPORTED_TASKS.keys())))
+
+    targeted_task = SUPPORTED_TASKS[task]
+    task_class = targeted_task["impl"]
+
+    if model is None:
+        model = targeted_task["default"]["model"]
+
+    if tokenizer is None:
+        if isinstance(model, str):
+            tokenizer = model
+        else:
+
+            raise Exception(
+                "Impossible to guess which tokenizer to use. "
+                "Please provided a PretrainedTokenizer class or a path/identifier to a pretrained tokenizer."
+            )
+
+    if isinstance(tokenizer, (str, tuple)):
+        if isinstance(tokenizer, tuple):
+
+            tokenizer = AutoTokenizer.from_pretrained(tokenizer[0], **tokenizer[1])
+        else:
+            tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+
+    if isinstance(model, str):
+        model = AutoModelForSeq2SeqLM.from_pretrained(model)
+
+    if task == "question-generation":
+        if ans_model is None:
+
+            ans_model = targeted_task["default"]["ans_model"]
+            ans_tokenizer = AutoTokenizer.from_pretrained(ans_model)
+            ans_model = AutoModelForSeq2SeqLM.from_pretrained(ans_model)
+        else:
+
+            if ans_tokenizer is None:
+                if isinstance(ans_model, str):
+                    ans_tokenizer = ans_model
+                else:
+
+                    raise Exception(
+                        "Impossible to guess which tokenizer to use. "
+                        "Please provided a PretrainedTokenizer class or a path/identifier to a pretrained tokenizer."
+                    )
+
+            if isinstance(ans_tokenizer, (str, tuple)):
+                if isinstance(ans_tokenizer, tuple):
+
+                    ans_tokenizer = AutoTokenizer.from_pretrained(ans_tokenizer[0], **ans_tokenizer[1])
+                else:
+                    ans_tokenizer = AutoTokenizer.from_pretrained(ans_tokenizer)
+
+            if isinstance(ans_model, str):
+                ans_model = AutoModelForSeq2SeqLM.from_pretrained(ans_model)
+
+    if task == "e2e-qg":
+        return task_class(model=model, tokenizer=tokenizer, use_cuda=use_cuda)
+    elif task == "question-generation":
+        return task_class(
+            model=model,
+            tokenizer=tokenizer,
+            ans_model=ans_model,
+            ans_tokenizer=ans_tokenizer,
+            qg_format=qg_format,
+            use_cuda=use_cuda,
+        )
+    else:
+        return task_class(
+            model=model,
+            tokenize=tokenizer,
+            ans_model=model,
+            ans_tokenizer=tokenizer,
+            qg_format=qg_format,
+            use_cuda=use_cuda,
+        )
